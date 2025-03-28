473a3e7ebf9abde9d9b0ec134fdffc2ac03c9afc
matsufriends <pikapikan0526@gmail.com>
2025-03-29 01:45:38 +0900

    Preview表示復活

--- ScriptableObject/MornNovelPoseSo.cs
+++ ScriptableObject/MornNovelPoseSo.cs
@@ -9,5 +9,8 @@ namespace MornNovel
         public MornNovelTalkerSo Talker;
         [SpritePreview(100f)] public Sprite DefaultSprite;
         public List<MornNovelPoseAnimation> Animations;
+        
+        // ViewableSearchAttributeで表示する用
+        public Sprite Preview => DefaultSprite;
     }
 }
