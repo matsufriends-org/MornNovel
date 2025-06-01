64f4433b9fb3bcaba0a4939174e2b815c6cc2ac7
matsufriends <pikapikan0526@gmail.com>
2025-06-02 03:01:03 +0900

    CameraShakeの仕組み更新

--- Command/MornNovelCameraShakeCommand.cs
+++ Command/MornNovelCameraShakeCommand.cs
@@ -3,6 +3,7 @@ using Arbor;
 using Cysharp.Threading.Tasks;
 using Unity.Cinemachine;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace MornNovel
 {
@@ -11,6 +12,9 @@ namespace MornNovel
         public override string Tips => "カメラを揺らす";
         [SerializeField] private CinemachineImpulseSource _source;
         [SerializeField] private StateLink _onComplete;
+        public float ShakeStrength = 1f;
+        public int ShakeCount = 5;
+        public float ShakeInterval = 0.05f;
 
         public override async void OnStateBegin()
         {
@@ -18,14 +22,27 @@ namespace MornNovel
             if (_source != null)
             {
                 var source = Instantiate(_source);
-                source.GenerateImpulse();
-                await UniTask.Delay(
-                    TimeSpan.FromSeconds(source.ImpulseDefinition.ImpulseDuration),
-                    cancellationToken: CancellationTokenOnEnd);
+                await ShakeAsync();
                 Destroy(source.gameObject);
             }
 
             Transition(_onComplete);
+        }
+
+        private async UniTask ShakeAsync()
+        {
+            for (var i = 0; i < ShakeCount; i++)
+            {
+                var randomDir = Random.insideUnitCircle.normalized;
+                var shakeVector = new Vector3(randomDir.x, randomDir.y, 0f) * ShakeStrength;
+
+                var impulse = _source.ImpulseDefinition;
+                impulse.ImpulseDuration = ShakeInterval;
+                _source.ImpulseDefinition = impulse;
+                
+                _source.GenerateImpulse(shakeVector);
+                await UniTask.Delay(TimeSpan.FromSeconds(ShakeInterval), cancellationToken: CancellationTokenOnEnd);
+            }
         }
     }
 }
