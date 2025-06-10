62ada41c6d52c2dbf127ccd5ec9d45d832c79b1c
matsufriends <pikapikan0526@gmail.com>
2025-06-11 02:27:17 +0900

    カメラシェイクに継続を追加

--- Command/MornNovelCameraShakeCommand.cs
+++ Command/MornNovelCameraShakeCommand.cs
@@ -11,37 +11,43 @@ namespace MornNovel
     public class MornNovelCameraShakeCommand : MornNovelCommandBase
     {
         public override string Tips => "カメラを揺らす";
+        [SerializeField, Label("揺らし続ける")] private bool _isLoop;
         [SerializeField] private MornNovelCameraShakeSettings _settings;
         [SerializeField] private StateLink _onComplete;
         [Inject] private MornNovelSettings _novelSettings;
 
-
         public override async void OnStateBegin()
         {
             base.OnStateBegin();
-            if (_novelSettings.SourcePrefab != null)
+            try
             {
-                var source = Instantiate(_novelSettings.SourcePrefab);
-                await ShakeAsync(source);
-                Destroy(source.gameObject);
-            }
+                if (_novelSettings.SourcePrefab != null)
+                {
+                    var source = Instantiate(_novelSettings.SourcePrefab);
+                    await ShakeAsync(source);
+                    Destroy(source.gameObject);
+                }
 
-            Transition(_onComplete);
+                Transition(_onComplete);
+            }
+            catch (OperationCanceledException)
+            {
+            }
         }
 
         private async UniTask ShakeAsync(CinemachineImpulseSource source)
         {
-            for (var i = 0; i < _settings.ShakeCount; i++)
+            for (var i = 0; i < _settings.ShakeCount || _isLoop; i++)
             {
                 var randomDir = Random.insideUnitCircle.normalized;
                 var shakeVector = new Vector3(randomDir.x, randomDir.y, 0f) * _settings.ShakeStrength;
-
                 var impulse = source.ImpulseDefinition;
                 impulse.ImpulseDuration = _settings.ShakeInterval;
                 source.ImpulseDefinition = impulse;
-                
                 source.GenerateImpulse(shakeVector);
-                await UniTask.Delay(TimeSpan.FromSeconds(_settings.ShakeInterval), cancellationToken: CancellationTokenOnEnd);
+                await UniTask.Delay(
+                    TimeSpan.FromSeconds(_settings.ShakeInterval),
+                    cancellationToken: CancellationTokenOnEnd);
             }
         }
     }
